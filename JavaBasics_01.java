5f6cea23bf889e4c4cee27bbf16d99369ede0318
Hrusikesh123 <hrushikeshs113@gmail.com>
2025-10-29 01:45:08 +0530

    first-commit

--- JavaBasics_01.java
+++ JavaBasics_01.java
@@ -1,4 +1,4 @@
-public class JavaBasics {
+public class JavaBasics_01 {
     public static void main(String args[]) {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
---
+++ javaBasics_02.java
@@ -0,0 +1,5 @@
+public class javaBasics_02 {
+    public static void main(String args[]) {
+        System.out.println("****\n***\n**\n*");
+    }
+}
