e60dd9b207e86cdfe35a3e357e032936abe6cec4
Hrusikesh123 <hs2068672@gmail.com>
2025-12-23 23:34:06 +0530

    update-commit

---
+++ Loops/While_loop_01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class While_loop_01 {
+    public static void main(String[] args) {
+        int couter = 0;
+        while(couter < 100) {
+            System.out.println("Hello World");
+            couter++;
+        }
+        System.out.println("Printed Hello World 100 times");
+    }
+}
