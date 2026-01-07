762a3c02cfbc7052556ded22d045b98719831de8
Hrusikesh123 <hs2068672@gmail.com>
2026-01-08 01:46:18 +0530

    Floyds Triangle

---
+++ Paterns(Part-2)/Floyds_Triangle_04.java
@@ -0,0 +1,16 @@
+public class Floyds_Triangle_04 {
+    public static void floyd_triangle(int n) {
+        int num = 1;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        floyd_triangle(5);
+    }
+}
