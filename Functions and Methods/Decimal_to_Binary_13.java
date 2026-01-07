2d1aa1326a728d31dd9bd7b104eb88ad3e2fea59
Hrusikesh123 <hs2068672@gmail.com>
2026-01-07 13:14:31 +0530

    Conversion-commit

---
+++ Functions and Methods/Decimal_to_Binary_13.java
@@ -0,0 +1,19 @@
+public class Decimal_to_Binary_13 {
+    public static void decToBin(int decNum) {
+        int myNum = decNum;
+        int pow = 0;
+        int binNum = 0;
+
+        while(decNum > 0) {
+            int rem = decNum % 2;
+            binNum = binNum + (rem * (int)Math.pow(10, pow));
+
+            pow++;
+            decNum = decNum / 2;
+        }
+        System.out.println("binary of " + myNum + " = " + binNum);
+    }
+    public static void main(String[] args) {
+        decToBin(11);
+    }
+}
