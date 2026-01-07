6938033fcd9d28ffa4053bf75892fd43157f98a7
Hrusikesh123 <hs2068672@gmail.com>
2026-01-07 21:48:16 +0530

    Hollow Rectangle Paterns

---
+++ Paterns(Part-2)/Hollow_Rectangle_Paterns_01.java
@@ -0,0 +1,21 @@
+public class Hollow_Rectangle_Paterns_01 {
+    public static void hollow_rectangle(int rows, int cols) {
+        //outer loop
+        for(int i=1; i<=rows; i++) {
+            //inner loop
+            for(int j=1; j<=cols; j++) {
+                //cell -> (i,j)
+                if(i==1 || i==rows || j==1 || j==cols) {
+                    //boundary cells
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); 
+        }
+    }
+    public static void main(String[] args) {
+        hollow_rectangle(4, 5);
+    }
+}
