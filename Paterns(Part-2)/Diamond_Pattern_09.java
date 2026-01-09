bb6729d9b4dd6ba5935c887103e1647a63b31513
Hrusikesh123 <hs2068672@gmail.com>
2026-01-09 23:13:32 +0530

    Diamond Pattern

---
+++ Paterns(Part-2)/Diamond_Pattern_09.java
@@ -0,0 +1,25 @@
+public class Diamond_Pattern_09 {
+    public static void main(String[] args) {
+        int n = 4;
+        // Upper half of the diamond
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // Lower half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
