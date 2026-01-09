13e6051225f40ac71f238ddfc1b44e87b2d00ace
Hrusikesh123 <hs2068672@gmail.com>
2026-01-09 23:08:52 +0530

    Array Creation

---
+++ Arrays(Part-1)/Arrays_Creation_01.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Arrays_Creation_01 {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+
+        Scanner sc = new Scanner(System.in);
+    }
+}
