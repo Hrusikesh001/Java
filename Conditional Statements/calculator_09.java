d952c5c4fd34140ecd211a663f579d76a03baa27
Hrusikesh123 <hs2068672@gmail.com>
2026-03-28 15:34:18 +0530

    Update calculator_09.java

--- Conditional Statements/calculator_09.java
+++ Conditional Statements/calculator_09.java
@@ -3,6 +3,32 @@ import java.util.Scanner;
 public class calculator_09 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.println("Enter a: ");
+        int a = scanner.nextInt();
+        System.out.println("Enter b: ");
+        int b = scanner.nextInt();
+        System.out.println("Enter operator (+, -, *, /, %): ");
+
+        char operator = scanner.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            case '%':
+                System.out.println(a % b);
+                break;
+            default:
+                System.out.println("Invalid Operator");
+        }
     }
 }
