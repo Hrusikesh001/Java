60bf0778905753739b7f4790b631002089a7c729
Hrusikesh123 <hs2068672@gmail.com>
2026-03-29 03:30:06 +0530

    pattern question

---
+++ Paterns(part-1)/Question_07.java
@@ -0,0 +1,16 @@
+public class Question_07 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < 10-(2*i+1); k++){
+                System.out.print("*");
+            }
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
