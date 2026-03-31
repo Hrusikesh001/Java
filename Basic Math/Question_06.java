529ce27956e496a1b9a96a72e4a94457e6430623
Hrusikesh123 <hs2068672@gmail.com>
2026-03-31 23:55:08 +0530

    Euclidean Algorithm

---
+++ Basic Math/Question_06.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Question_06 {
+    public static void main(String[] args) {
+        int a = 48, b = 18;
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("GCD = " + a);
+    }
+}
---
+++ Basic Math/Question_07.java
@@ -0,0 +1,15 @@
+public class Question_07 {
+    public static void main(String[] args) {
+        int a = 48, b = 18;
+
+        while (a>0 && b>0) {
+            if (a > b) a = a % b;
+            else b = b % a;
+        }
+        if(a == 0) {
+            System.out.println("GCD = " + b);
+        } else {
+            System.out.println("GCD = " + a);
+        }
+    }
+}
