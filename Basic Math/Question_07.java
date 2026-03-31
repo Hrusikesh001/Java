0189179da85eb09f951f7e1c7935e192f5178366
Hrusikesh123 <hs2068672@gmail.com>
2026-04-01 02:10:32 +0530

    Iterative Euclidean Algorithm

--- Basic Math/Question_07.java
+++ Basic Math/Question_07.java
@@ -1,15 +1,29 @@
-public class Question_07 {
-    public static void main(String[] args) {
-        int a = 48, b = 18;
+// public class Question_07 {
+//     public static void main(String[] args) {
+//         int a = 48, b = 18;
 
-        while (a>0 && b>0) {
-            if (a > b) a = a % b;
-            else b = b % a;
-        }
-        if(a == 0) {
-            System.out.println("GCD = " + b);
-        } else {
-            System.out.println("GCD = " + a);
-        }
+//         while (a>0 && b>0) {
+//             if (a > b) a = a % b;
+//             else b = b % a;
+//         }
+//         if(a == 0) {
+//             System.out.println("GCD = " + b);
+//         } else {
+//             System.out.println("GCD = " + a);
+//         }
+//     }
+// }
+
+
+public class Question_07 {
+      public static void main(String[] args) {
+          int a = 48, b = 18;
+          while (b != 0) {
+              int temp = b;
+              b = a % b;
+              a = temp;
+          }
+
+          System.out.println("GCD = " + a);
+      }
     }
-}
