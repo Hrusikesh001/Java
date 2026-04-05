8d502e55df3595bf677e3ec028eff16b954554ea
Hrusikesh123 <hs2068672@gmail.com>
2026-04-05 23:46:12 +0530

    updated

--- Paterns(part-1)/Question_18.java
+++ Paterns(part-1)/Question_18.java
@@ -1,22 +1,51 @@
-import java.util.*;
 public class Question_18 {
     public static void main(String[] args) {
         int n = 6;
         int inis = 0;
-        for(int i = 0; i < n; i++) {
-            // Stars
-            for(int j = 1; j < n-i; j++) {
+
+        // Upper part
+        for (int i = 0; i < n; i++) {
+
+            // stars
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print("*");
-        }
-            // Spaces
-            for(int j = 0; j <inis; j++) {
+            }
+
+            // spaces
+            for (int j = 0; j < inis; j++) {
                 System.out.print(" ");
             }
-            // Stars
-            for(int j = 1; j < n-i; j++) {
+
+            // stars
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print("*");
             }
+
             inis += 2;
+            System.out.println();
+        }
+
+        // Lower part
+        inis = 2 * (n - 1);
+
+        for (int i = 1; i <= n; i++) {
+
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // spaces
+            for (int j = 0; j < inis; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            inis -= 2;
             System.out.println();
         }
     }
