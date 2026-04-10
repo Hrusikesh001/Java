260cbad320a6f4f57ec9693e2aa09d63c14d08dd
Hrusikesh123 <hs2068672@gmail.com>
2026-04-10 11:20:12 +0530

    Bubble Sort

--- Basic Sorting Algorithm/Bubble_Sort_01.java
+++ Basic Sorting Algorithm/Bubble_Sort_01.java
@@ -12,9 +12,15 @@ public class Bubble_Sort_01 {
             }
         }
     }
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String args[]) {
         int arr[] = {5, 4, 1, 3, 2};
         bubbleSort(arr);
-        System.out.println("Sorted array: " + Arrays.toString(arr));
+        printArray(arr);
     }
 }
