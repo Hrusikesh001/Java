ac0f67e8b797d7c87ff467a15c26a98e1ceb0dad
Hrusikesh123 <hs2068672@gmail.com>
2026-04-17 03:00:18 +0530

    feat: add String_01 class to demonstrate basic string initialization and input handling

---
+++ Strings/String_01.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class String_01 {
+    public static void main(String[] args) {
+        char arr[] = {'H', 'e', 'l', 'l', 'o'};
+        String str = "abcde";
+        String str2 = new String("abcde");
+
+        //Strings are immutable
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println(name);
+
+    }
+}
