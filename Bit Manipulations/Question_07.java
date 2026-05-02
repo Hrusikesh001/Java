579fa1e740298524b4a8ddc49874ba905cec079b
Hrusikesh123 <hs2068672@gmail.com>
2026-05-02 22:23:36 +0530

    feat: add bit manipulation method to determine if a number is odd or even

---
+++ Bit Manipulations/Question_07.java
@@ -0,0 +1,15 @@
+public class Question_07 {
+    public static void oddOrEven(int n) {
+        int bitMask = 1;
+        if ((n & bitMask) == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+    public static void main(String[] args) {
+        oddOrEven(3); 
+        oddOrEven(11); 
+        oddOrEven(14);
+    }
+}
