fe993b4c5ca5aaed574c117f9070559d91494035
Hrusikesh123 <hs2068672@gmail.com>
2026-05-10 12:59:54 +0530

    feat: add initial Pen and Student class definitions with basic attribute and method implementations

---
+++ OOPS/Classes_Objects_01.java
@@ -0,0 +1,33 @@
+public class Classes_Objects_01 {
+    public static void main(String[] args) {
+
+        Pen p1 = new Pen();
+
+        p1.setColor("Blue");
+
+        System.out.println(p1.color);
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    float percentage;
+
+    void calculatePercentage(int phy, int chem, int math) {
+        percentage = (phy + chem + math) / 3.0f;
+    }
+}
