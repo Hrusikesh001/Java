b2ca60f60d51b72a3e57dece2fde31323bfecea5
Hrusikesh123 <hs2068672@gmail.com>
2026-05-10 12:28:11 +0530

    feat:  implementation for efficient power calculation

---
+++ Bit Manipulations/Fast_Exponentiation_15.java
@@ -0,0 +1,18 @@
+public class Fast_Exponentiation_15 {
+    public static int fastExponentiation(int a, int n) {
+        int ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                ans = ans * a;
+            }
+            a = a * a;
+            n = n >> 1;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(fastExponentiation(2, 5)); 
+        System.out.println(fastExponentiation(3, 4));
+        System.out.println(fastExponentiation(5, 3));  
+    }
+}
