a525f41513c42b455000118c75cd9b7bd1fb9055
Hrusikesh123 <hs2068672@gmail.com>
2026-05-23 11:55:45 +0530

    feat: add Static_keyword_16 example demonstrating static variables and methods in a Student class

---
+++ OOPS/Static_keyword_16.java
@@ -0,0 +1,29 @@
+public class Static_keyword_16 {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.schoolName = "SSVM";
+
+        Student s2 = new Student();
+        System.out.println(s2.schoolName);
+
+        Student s3 = new Student();
+        s3.schoolName = "ABC";
+    }
+}
+
+class Student {
+    static int returnPercentage(int math, int phy, int chem) {
+        return (math + phy + chem) / 3;
+    } 
+    String name;
+    int rollNo;
+
+    static String schoolName; 
+
+    void setName(String name) {
+        this.name = name;
+    }
+    String getName() {
+        return this.name;
+    }
+}
