7721219696dd3aad687013e1fc48dc82a889db7b
Hrusikesh123 <hs2068672@gmail.com>
2026-05-25 15:17:57 +0530

    feat: add recursion example to print numbers from n to 1 in decreasing order

---
+++ Recursion Basics(Part-1)/Question_01.java
@@ -0,0 +1,16 @@
+//Print numbers from n to 1 in decreasing order using recursion.
+public class Question_01 {
+    public static void printDec(int n) {
+        if(n == 1 ) {
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n + " ");
+        printDec(n-1);
+    }
+
+public static void main (String[] args) {
+    int n = 10;
+    printDec(n);
+    }
+}
