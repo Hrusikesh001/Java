6879d14680abe22ba18c1db0b04a99bb918090fc
Hrusikesh123 <hs2068672@gmail.com>
2026-05-28 21:21:59 +0530

    feat: add Remove Duplicates solution using recursion

---
+++ Recursion(Part-2)/Remove_Duplicates_02.java
@@ -0,0 +1,24 @@
+
+
+public class Remove_Duplicates_02 {
+    public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+        if (idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if (map[currChar - 'a'] == true) {
+            //duplicate
+            removeDuplicates(str, idx + 1, newStr, map);
+        } else {
+            //not duplicate
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, idx + 1, newStr.append(currChar), map);
+        }
+    }
+        public static void main(String[] args) {
+            String str = "appnnacollege";
+            boolean map[] = new boolean[26];
+            removeDuplicates(str, 0, new StringBuilder(""), map);
+        }
+}
