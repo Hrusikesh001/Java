fbfe63c4047ac071fe7e5affb82aec8b8de2a130
Hrusikesh123 <hs2068672@gmail.com>
2026-06-12 12:03:07 +0530

    feat: add initial implementation of Backtracking_on_Array_01 with array manipulation methods

---
+++ ArrayList/Introduction_01.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Introduction_01 {
+    public static void main(String args[]) {
+        //ClassName objectName = new ClassName();
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>();
+    }
+}
---
+++ Backtracking/Backtracking_on_Array_01.java
@@ -0,0 +1,16 @@
+public class Backtracking_on_Array_01 {
+    public static void changeArr(int arr[], int i, int val) {
+        
+    }
+    public static void printArr(int arr[]) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        changeArr(arr, 0, 1);
+        printArr(arr);
+    }
+}
