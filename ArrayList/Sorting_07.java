9478cc665e4ab1f1b62b4a107d2b04073d5bb1f2
Hrusikesh123 <hs2068672@gmail.com>
2026-06-13 19:14:38 +0530

    feat: add Sorting_07 implementation to sort an ArrayList in ascending and descending order

---
+++ ArrayList/Sorting_07.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class Sorting_07 {
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(6);
+        list.add(1);
+
+        System.out.println(list);
+        // Sort the arraylist in ascending order
+        Collections.sort(list);
+        System.out.println(list);
+
+        // Sort the arraylist in descending order
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println(list);
+    }
+    
+}
