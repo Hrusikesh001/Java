69bee7d2f7454e49fe5226f5cd79acb7344a87e1
Hrusikesh123 <hs2068672@gmail.com>
2026-06-14 23:26:25 +0530

    feat: update Pair_sum_1_11 to use two-pointer approach for pair sum check

--- ArrayList/Pair_sum_1_11.java
+++ ArrayList/Pair_sum_1_11.java
@@ -1,11 +1,27 @@
 import java.util.ArrayList;
 public class Pair_sum_1_11 {
-    public static boolean pairSum(ArrayList<Integer> list, int target) {
-        for(int i=0; i<list.size(); i++) {
-            for(int j=i+1; j<list.size(); j++) {
-                if(list.get(i) + list.get(j) == target) {
-                    return true;
-                }
+    // public static boolean pairSum(ArrayList<Integer> list, int target) {
+    //     for(int i=0; i<list.size(); i++) {
+    //         for(int j=i+1; j<list.size(); j++) {
+    //             if(list.get(i) + list.get(j) == target) {
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    //2 pointer approach
+    public static boolean pairSum1(ArrayList<Integer> list, int target) {
+        int left = 0;
+        int right = list.size() - 1;
+        while(left != right) {
+            if(list.get(left) + list.get(right) == target) {
+                return true;
+            } else if(list.get(left) + list.get(right) < target) {
+                left++;
+            } else {
+                right--;
             }
         }
         return false;
@@ -20,6 +36,6 @@ public class Pair_sum_1_11 {
         list.add(6);
         
         int target = 3;
-        System.out.println(pairSum(list, target));
+        System.out.println(pairSum1(list, target));
     }
 }
