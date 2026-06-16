b73b9d5fc1798572d7cba6998bd53ceb6a03e134
Hrusikesh123 <hs2068672@gmail.com>
2026-06-17 01:34:43 +0530

    feat: implement addFirst method to add nodes at the beginning of the linked list

--- Linked Lists(Part 1)/Introduction_01.java
+++ Linked Lists(Part 1)/Introduction_01.java
@@ -11,7 +11,25 @@ public class Introduction_01 {
     public static Node head;
     public static Node tail;
 
+    public static void addFirst(int data) {
+        // step 1 - create new node
+        Node newNode = new Node(data);
+
+        if(head == null) {
+            head = tail = new Node(data);
+            return;
+        }
+        
+        // step 2 - newNode next = head
+        newNode.next = head;
+
+        // step 3 - head = newNode
+        head = newNode;
+    }
+
     public static void main(String args[]) {
-        LinkedList ll = new LinkedList();
+        Introduction_01 ll = new Introduction_01();
+        ll.addFirst(1);
+        ll.addFirst(2);
     }
 }
