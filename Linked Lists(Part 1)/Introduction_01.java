e6d8e10814965dca3af10dc01e15050d06ec87d8
Hrusikesh123 <hs2068672@gmail.com>
2026-06-17 01:57:18 +0530

    feat: refactor addFirst method and implement addLast method for linked list

--- Linked Lists(Part 1)/Introduction_01.java
+++ Linked Lists(Part 1)/Introduction_01.java
@@ -14,22 +14,33 @@ public class Introduction_01 {
     public static void addFirst(int data) {
         // step 1 - create new node
         Node newNode = new Node(data);
-
         if(head == null) {
             head = tail = new Node(data);
             return;
         }
-        
         // step 2 - newNode next = head
         newNode.next = head;
-
         // step 3 - head = newNode
         head = newNode;
+    }
+    public static void addLast(int data) {
+        // step 1 - create new node
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = tail = new Node(data);
+            return;
+        }
+        // step 2 - tail next = newNode
+        tail.next = newNode;
+        // step 3 - tail = newNode
+        tail = newNode;
     }
 
     public static void main(String args[]) {
         Introduction_01 ll = new Introduction_01();
-        ll.addFirst(1);
         ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
     }
 }
