d35e733eb214461dac44d63fb240d071daf6b4bf
Hrusikesh123 <hs2068672@gmail.com>
2026-06-18 15:53:44 +0530

    feat: implement iterative search method in linked list and update main method for demonstration

--- Linked Lists(Part 1)/Introduction_01.java
+++ Linked Lists(Part 1)/Introduction_01.java
@@ -109,6 +109,20 @@ public class Introduction_01 {
         return val;
     }
 
+    public int itrSearch(int key) {
+        Node temp = head;
+        int i = 0;
+        while(temp != null) {
+            if(temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        //key not found
+        return -1;
+    }
+
     public static void main(String args[]) {
         Introduction_01 ll = new Introduction_01();
         ll.addFirst(2);
@@ -124,5 +138,8 @@ public class Introduction_01 {
         ll.removeLast();
         ll.print();
         System.out.println(ll.size);
+
+        System.out.println(ll.itrSearch(3));
+        System.out.println(ll.itrSearch(10));
     }
 }
