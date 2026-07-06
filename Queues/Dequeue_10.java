41fd97ea13d57d9d7c8eb8901b8ad8ca9e9f859e
Hrusikesh123 <hs2068672@gmail.com>
2026-07-06 23:20:50 +0530

    feat: add Dequeue_10 class with main method to demonstrate deque operations

---
+++ Queues/Dequeue_10.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Dequeue_10 {
+    public static void main(String[] args) {
+        Deque<Integer> dq = new LinkedList<>();
+        dq.addFirst(1);
+        dq.addFirst(2);
+        dq.addLast(3);
+        dq.addLast(4);
+        System.out.println(dq);
+        dq.removeFirst();
+        System.out.println(dq);
+    }
+}
