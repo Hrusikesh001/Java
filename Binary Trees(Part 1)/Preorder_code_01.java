5c5fdfbab323a8d20fa0ded2c659c84215f7cc71
Hrusikesh123 <hs2068672@gmail.com>
2026-07-07 15:47:03 +0530

    feat: add inorder traversal method to BinaryTree class

--- Binary Trees(Part 1)/Preorder_code_01.java
+++ Binary Trees(Part 1)/Preorder_code_01.java
@@ -36,6 +36,16 @@ public class Preorder_code_01 {
             preorder(root.left);
             preorder(root.right);
         }
+
+        // Inorder traversal: left -> root -> right
+        public static void inorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -44,6 +54,9 @@ public class Preorder_code_01 {
         System.out.println(root.data);
         System.out.print("Preorder traversal: ");
         tree.preorder(root);
+        System.out.println();
+        System.out.print("Inorder traversal: ");
+        tree.inorder(root);
         System.out.println();
     }
 }
