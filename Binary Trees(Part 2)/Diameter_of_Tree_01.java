fe8287e33881939da964164bd5b4a0d584e1d6b1
Hrusikesh123 <hs2068672@gmail.com>
2026-07-08 00:44:59 +0530

    feat: add second approach for diameter calculation in Diameter_of_Tree_01 class

--- Binary Trees(Part 2)/Diameter_of_Tree_01.java
+++ Binary Trees(Part 2)/Diameter_of_Tree_01.java
@@ -33,6 +33,31 @@ public class Diameter_of_Tree_01 {
         return Math.max(leftHeight, rightHeight) + 1;
     }
 
+    //2nd approach
+
+    static class Info {
+        int diameter;
+        int height;
+
+        public Info(int diameter, int height) {
+            this.diameter = diameter;
+            this.height = height;
+        }
+    }
+    public static Info diameter2(Node root) {
+        if (root == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter2(root.left);
+        Info rightInfo = diameter2(root.right);
+
+        int diameter = Math.max(Math.max(leftInfo.diameter, rightInfo.diameter),
+                leftInfo.height + rightInfo.height + 1);
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+
+        return new Info(diameter, height);
+    }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -42,5 +67,6 @@ public class Diameter_of_Tree_01 {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
         System.out.println("Diameter of the tree: " + diameter(root));
+        System.out.println("Diameter of the tree (2nd approach): " + diameter2(root).diameter);
     }
 }
