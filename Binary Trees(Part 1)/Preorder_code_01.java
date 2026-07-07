1ad6360354f437bbf2eb1cb19e1c440b1478db90
Hrusikesh123 <hs2068672@gmail.com>
2026-07-07 15:45:05 +0530

    feat: add preorder traversal method to BinaryTree class

--- Binary Trees(Part 1)/Preorder_code_01.java
+++ Binary Trees(Part 1)/Preorder_code_01.java
@@ -26,12 +26,25 @@ public class Preorder_code_01 {
 
             return newNode;
         }
+
+        // Preorder traversal: root -> left -> right
+        public static void preorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
-
+        System.out.print("Preorder traversal: ");
+        tree.preorder(root);
+        System.out.println();
     }
 }
+
