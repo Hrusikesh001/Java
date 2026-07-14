03a3193ca76165ba301cfca58e46f41588a51249
Hrusikesh123 <hs2068672@gmail.com>
2026-07-14 22:34:53 +0530

    feat: update PQ_in_JCF_01 class to use Student objects in PriorityQueue

--- Heaps/PQ_in_JCF_01.java
+++ Heaps/PQ_in_JCF_01.java
@@ -1,14 +1,27 @@
 import java.util.PriorityQueue;
 public class PQ_in_JCF_01 {
+    public static class Student implements Comparable<Student> {
+        String name;
+        int rank;
+
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+    }
     public static void main(String[] args) {
-        // PriorityQueue<Integer> pq = new PriorityQueue<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(java.util.Collections.reverseOrder());
-        pq.add(3);
-        pq.add(4);
-        pq.add(1);
-        pq.add(7);
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(java.util.Collections.reverseOrder());
+        pq.add(new Student("Alice", 3));
+        pq.add(new Student("Bob", 4));
+        pq.add(new Student("Charlie", 1));
+        pq.add(new Student("David", 7));
         while (!pq.isEmpty()) {
-            System.out.println(pq.peek());
+            System.out.println(pq.peek().name + " " + pq.peek().rank);
             pq.remove();
         }
     }
