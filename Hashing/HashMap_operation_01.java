7dda44d91d71c4d19cbe0020a7c2b3b505d051ee
Hrusikesh123 <hs2068672@gmail.com>
2026-07-16 23:38:55 +0530

    feat: add HashMap_operation_01 class to demonstrate basic HashMap operations

---
+++ Hashing/HashMap_operation_01.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HashMap_operation_01 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        //Insert
+        map.put("India", 100);
+        map.put("China", 200);
+        map.put("USA", 300);
+
+        System.out.println(map);
+    }
+}
